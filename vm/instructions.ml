e658af9c664d19a05a12e76237154189e7e9c2ba
minishrink <mathur_akanksha@hotmail.com>
2019-04-19 20:34:28 +0100

    Functorise instructions
    Signed-off-by: minishrink <mathur_akanksha@hotmail.com>

--- vm/instructions.ml
+++ vm/instructions.ml
@@ -1,50 +1,44 @@
+module OPCODES (V : Machine.Value) = struct
 
-type t = int
-type memory
+  type var_op =
+    | BIPUSH of V.t
+    | SIPUSH of V.t
+    | CONST of V.t
 
+  type mem_op =
+    | LOAD of V.m
+    | STORE of V.m
+    | INC of V.m * V.t
 
-type 't var_op =
-   | BIPUSH of 't
-   | SIPUSH of 't
-   | CONST of 't
-
-type 'm mem_op =
-  | LOAD of 'm
-  | STORE of 'm
-
-type binary_op =
-  (** pop top
-   *  pop next
-   *  perform op
-   *  push result
-   **)
-  | ADD
-  | SUB
-  | MUL
-  | DIV
-  | REM
-  | AND
-  | OR
-  | XOR
-  | LSHIFT
-  | RSHIFT
- (* | U_LSHIFT
+  type binary_op =
+    | ADD
+    | SUB
+    | MUL
+    | DIV
+    | REM
+    | AND
+    | OR
+    | XOR
+    | LSHIFT
+    | RSHIFT
+(*  | U_LSHIFT
     | U_RSHIFT
  *)
 
-type unary_op =
-  | NEG
+  type unary_op =
+    | NEG
 
-type component =
-  | Alu
-  | Mem
-  | Stk
-  | Var
+  type component =
+    | Alu
+    | Mem
+    | Stk
+    | Var
 
-type ('t, 'm) opcode
+  type opcode
 
-type ('t, 'm) instruction =
-  { opcode    : ('t, 'm) opcode
-  ; component : component
-  ; cycles    : int
-  }
+  type instruction =
+    { opcode    : opcode
+    ; component : component
+    ; cycles    : int
+    }
+end
