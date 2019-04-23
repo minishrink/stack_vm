f047999c88207c7223687360a5eb3076262bb6bd
minishrink <mathur_akanksha@hotmail.com>
2019-04-23 02:26:11 +0100

    Add more string trim functions
    Signed-off-by: minishrink <mathur_akanksha@hotmail.com>

--- lib/stringtrim.ml
+++ lib/stringtrim.ml
@@ -4,20 +4,36 @@ let get_lines = String.split_on_char '\n'
 
 let get_words = String.split_on_char ' '
 
-let remove_underscores line =
+let remove char line =
   let rec handle = function
   | hd :: tl ->
-    let hd = hd |> String.split_on_char '_' in
+    let hd = hd |> String.split_on_char char in
     hd @ (handle tl)
   | [] -> []
   in handle line
+
+let trim line =
+  line
+  |> remove '_'
+  |> remove ','
+
+let handle_branches str =
+  if String.length str < 3 then str
+  else begin
+    if String.sub str 0 3 = "if_"
+    then try
+      String.sub str 3 (String.length str - 3)
+    with _ -> str
+    else str
+end
 
 let tokenize line =
   line
   |> get_words
   |> clean
-  |> remove_underscores
-  |> clean
+  |> List.map handle_branches
+  |> trim
+  |> List.filter (fun str -> not (List.mem str [","; ":";".";""]))
 
 let get_instructions text =
   text
