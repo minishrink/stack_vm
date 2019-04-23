66807f2ad6ae85f3b3c273811c765fb8553d72b8
minishrink <mathur_akanksha@hotmail.com>
2019-04-23 01:06:10 +0100

    Fix underscores
    Signed-off-by: minishrink <mathur_akanksha@hotmail.com>

--- lib/stringtrim.ml
+++ lib/stringtrim.ml
@@ -5,20 +5,26 @@ let get_lines = String.split_on_char '\n'
 let get_words = String.split_on_char ' '
 
 let remove_underscores line =
-  match line with
+  let rec handle = function
   | hd :: tl ->
     let hd = hd |> String.split_on_char '_' in
-    hd @ tl
+    hd @ (handle tl)
   | [] -> []
+  in handle line
+
+let tokenize line =
+  line
+  |> get_words
+  |> clean
+  |> remove_underscores
+  |> clean
 
 let get_instructions text =
   text
   |> get_lines
-  |> List.map
-    (fun line -> line |> get_words |> clean)
-  |> List.filter
-    (function | [] -> false | _ -> true)
-  |> List.map remove_underscores
+  |> List.map tokenize
+  |> List.filter (function | [] -> false | _ -> true)
+  |> List.map List.tl
 
 let snippet = "
   iconst_9
